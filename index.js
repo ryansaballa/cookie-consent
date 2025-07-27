ac28c2c8d9f199df443ef78216367b8bca521e1f
perrystudios <73252481+perrystudios@users.noreply.github.com>
2025-07-27 18:11:02 -0400

    new form in modal

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const modal = document.getElementById("modal")
-
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+ 
 setTimeout(function(){
     modal.style.display = 'inline'
-},1500)
+}, 1500)
+
+
+modalCloseBtn.addEventListener("click", function(){
+    modal.style.display = 'none'   
+}
+)
